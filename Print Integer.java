43e83e6da0c28a8928f328e663aa637974fb85bc
bhum89 <143217734+bhum89@users.noreply.github.com>
2023-09-06 11:39:26 +0530

    Print Integer.java

---
+++ Print Integer.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class PrintInt{
+public static void main(String[]args){
+Scannersc=new Scanner(System.in);
+System.out.println("Enter a number");
+int n=sc.nextInt();
+System.out.println(n);
+}
+}
