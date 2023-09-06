691d5031112e83fb3354e0cd91936bf55d6f3c56
bhum89 <143217734+bhum89@users.noreply.github.com>
2023-09-06 23:10:58 +0530

    Natural number.java

---
+++ Natural number.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class NaturalNumber{
+public static void main(String[]args){
+  Scanner sc=newScanner(System.in);
+  System.out.println("Enter a number");
+  int n=sc.nextInt();
+  int Sum=0;
+  for(int i=1;i<=n;i++)
+    {
+        sum=sum+i;
+    }
+  System.out.println(sum);
+}
+}
