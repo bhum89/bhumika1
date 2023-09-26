d073ecb1cb5c84ff3e37f54e6ff4cc1a3476f7e3
bhum89 <143217734+bhum89@users.noreply.github.com>
2023-09-27 00:03:16 +0530

    String.java

---
+++ String.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[]args){
+        String first ="Java";
+        String second ="Python";
+        String third=" javaScript";
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+    }
+}
