2a5721a6c2ab0d881c1b170c63974a6aa2438d4b
bhum89 <143217734+bhum89@users.noreply.github.com>
2023-09-27 00:24:25 +0530

    boolean equal.java

---
+++ boolean equal.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args){
+        String first="Java programming ";
+        String second="Java programming";
+        String third="Javascript programming";
+     boolean result1=first.equals(second);
+        System.out.println("String first and second are equal:"+result1);
+        boolean result2=first.equals(third);
+        System.out.println("String first and third are equal:"+result2);
+    }
+}
