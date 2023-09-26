d5f7e8c0b12ded6f01a43801c0fa22b1959192d3
bhum89 <143217734+bhum89@users.noreply.github.com>
2023-09-27 00:19:17 +0530

    concat.java

---
+++ concat.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args){
+        String first ="Java";
+        System.out.println("First String:"+first);
+        String second="Programming";
+        System.out.println("Second String:"+second);
+        String joinedString =first.concat(second);
+        System.out.println("JoinedString:"+joinedString);
+    }
+}
