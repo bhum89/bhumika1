e088d0333f4503cdf06cb976d461833615d5ee47
bhum89 <143217734+bhum89@users.noreply.github.com>
2023-09-27 11:56:44 +0530

    String for loop .java

---
+++ String for loop .java
@@ -0,0 +1,11 @@
+//Demonstrate for each loop of an array
+
+public class Ex{
+public static void main(String []args){
+    String a[]={"Dog","Cat","man"};
+    for( String i : a)
+    {
+        System.out.println(i);
+    }
+}
+}
