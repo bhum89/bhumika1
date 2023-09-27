124ee85944c4cdbc17f5570b0aa54a2603b0d406
bhum89 <143217734+bhum89@users.noreply.github.com>
2023-09-27 11:41:40 +0530

    array.java

---
+++ array.java
@@ -0,0 +1,8 @@
+public class Array{
+public static void main(String []args){
+    int arr[]=new int [3];
+    int arr [3];
+    int arr[]=new int []{1,2,3,4};
+    int arr []=new int[3]{1,2,3};
+}
+}
