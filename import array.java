cf6bbd083ee2ac9a814d4a3eddb465465919c2d8
bhum89 <143217734+bhum89@users.noreply.github.com>
2023-09-27 12:12:21 +0530

    import array.java

---
+++ import array.java
@@ -0,0 +1,14 @@
+//WAP to find the minimum and maximum array in minimum code
+import java.util.Arrays;
+public class Ex{
+public static void main(String []args){
+    int a[]={10,45,29,56,7};
+    String s[]={"Dog","Cat","Man"};
+    System.out.println(Arrays.toString(a));
+    Arrays.sort(a);
+     System.out.println(Arrays.toString(a));
+     System.out.println(Arrays.toString(s));
+    Arrays.sort(s);
+    System.out.println(Arrays.toString(s));
+}
+}
