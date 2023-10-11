f2f0a6333299756332da9a744afb6b7b3ddd3bc6
bhum89 <143217734+bhum89@users.noreply.github.com>
2023-10-11 11:57:45 +0530

    overloading and overriding.java

---
+++ overloading and overriding.java
@@ -0,0 +1,26 @@
+class Parent{
+    void display(){
+        System.out.println("This is parent class");
+    }
+    void show(int num){
+        System.out.println("The number in the parent class is "+num);
+    }
+}
+class Child extends Parent{
+    void display(){
+        System.out.println("This class is the child class ");
+    }
+    void show(double num){
+        System.out.println("The number in the child class is "+num);
+    }
+}
+public class Main{
+    public static void main(String [] args){
+        Parent parentobj = new Parent();
+        Parent childobj =new Child();
+        parentobj.show(7);
+        childobj.show(75);
+        parentobj.display();
+        childobj.display();
+    }
+}
