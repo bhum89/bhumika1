f1840a8b8efd7121ae5e10c58b94f9eaae7c3a4b
bhum89 <143217734+bhum89@users.noreply.github.com>
2023-10-18 12:39:04 +0530

    parentexceptionclass.java

---
+++ parentexceptionclass.java
@@ -0,0 +1,13 @@
+public class errorChecking{
+    public static void main(String[]args){
+        try{
+            int x=10;
+            int y=0;
+            int z=x/y;
+            System.out.println(z);
+        }
+        catch(Throwable err){
+            System.out.println(err.getMessage());
+        }
+    }
+}
