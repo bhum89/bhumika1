41b66950bbc53a6ebe31c2a836d20d40635a28b8
bhum89 <143217734+bhum89@users.noreply.github.com>
2023-10-18 12:45:02 +0530

    finallyException.java

---
+++ finallyException.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[]args){
+        try{
+            int n;
+            Scanner sc =new Scanner(System.in);
+            System.out.println("Enter value");
+            n=sc.nextInt();
+            int divideByzero=5/n;
+            //System.out.println("Rest of code in try block");
+        }
+        catch(ArithmeticException e){
+            System.out.println("ArithmeticException =>"+e.getMessage());
+        }
+        finally{
+            System.out.println("This is finally block");
+        }
+    }
+}
